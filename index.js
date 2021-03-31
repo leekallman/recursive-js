ba83a3200a35d0c3e6b84432a7f792fec8a046b3
Lisa Lee <lisaleekallman@gmail.com>
2021-03-31 20:37:15 +0200

    exercise 2 - same as exercise 1 but fith factorial numbers - completed

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function sumValues(i) {
     }
 }
 console.log(sumValues(3));
+
+function sumValues(i) {
+    if (i <= 1) {
+        return i;
+    } else {
+        return i * sumValues(i - 1);
+    }
+}
+
+console.log(sumValues(5));
