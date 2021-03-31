cb12a5390d8ad298f2618dd2cc6af30d0cd3d1a1
Lisa Lee <lisaleekallman@gmail.com>
2021-03-31 20:35:46 +0200

    exercise 1 - sum all values up to a number, starting from 1

---
+++ index.js
@@ -0,0 +1,20 @@
+/* function sumValues(i) {
+  if (i < 1) {
+    return i;
+  } else {
+    return sumValues(i - 1) + i;
+  }
+}
+
+console.log(sumValues(3));
+ */
+
+function sumValues(i) {
+    if (i <= 1) {
+        return i;
+    } else {
+        return i * sumValues(i - 1);
+    }
+}
+
+console.log(sumValues(5));
