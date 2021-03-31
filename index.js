7ad0399d484503e163a0a0b6507e7a7ba32bcf26
Lisa Lee <lisaleekallman@gmail.com>
2021-03-31 20:36:07 +0200

    completed exercise 1

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-/* function sumValues(i) {
-  if (i < 1) {
-    return i;
-  } else {
-    return sumValues(i - 1) + i;
-  }
-}
-
-console.log(sumValues(3));
- */
-
 function sumValues(i) {
-    if (i <= 1) {
+    if (i < 1) {
         return i;
     } else {
-        return i * sumValues(i - 1);
+        return sumValues(i - 1) + i;
     }
 }
-
-console.log(sumValues(5));
+console.log(sumValues(3));
